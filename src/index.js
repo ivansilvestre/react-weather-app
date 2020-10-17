43f22c1389bd588d8c5b7c5d6c3cfc6443a356ff
ivansilvestre <ivanbsilvestre@gmail.com>
2020-10-17 21:14:31 +0100

    clean code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
@@ -12,4 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
