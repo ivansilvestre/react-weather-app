069ac22a3045782e9e6245464ae3150954b3ad65
ivansilvestre <ivanbsilvestre@gmail.com>
2020-12-19 15:45:18 +0000

    minor fix

--- src/components/card/MainCard.jsx
+++ src/components/card/MainCard.jsx
@@ -7,7 +7,7 @@ const MainCard = (props) => {
         <img src={props.img} alt="weather-img" />
       </div>
       <div className="text-muted text-uppercase text-center details">
-        <h5 className="my-2">City: {props.city} </h5>
+        <h5 className="my-2">{props.city} </h5>
         <div className="display-4 my-3">
           <span> {props.weather}</span>
           <span>&deg;C</span>
